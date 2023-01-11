33709305d9847ccc5d9a58f66547ba505565d95b
openhe <625529334@qq.com>
2023-01-11 13:59:22 +0800

    1-11-upd: update demo for spring 2&3

---
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// this is main class for a springboot application
+@SpringBootApplication
+public class DemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
---
+++ docs/note1.md
@@ -0,0 +1,9 @@
+# Springboot Note 1
+1. Springboot 优点
+    1. 创建独立spring应用
+    2. 内嵌web服务器，直接启动
+    3. starter依赖，简化配置 
+    4. 无代码生成，无需编写xml 
+    5. 自动配置spring及一些第三方应用 
+    6. 生产级别应用监控 
+2. 
